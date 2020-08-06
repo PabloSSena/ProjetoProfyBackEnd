4fa9669af8235f01f66200e7fe20427ad0b59fd2
PabloSSena <pablosilvasena@gmail.com>
2020-08-05 23:10:49 -0300

    Criando a conexao

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/users',(request,response) => {
-    return response.json("hELoooo")
+app.get('/',(request,response) => {
+
+    return response.json({message:'Hello World'})
+
 })
 
 app.listen(3333)
