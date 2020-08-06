41003294fb71dbc3ce029a44436e3f4cb567aaa7
PabloSSena <pablosilvasena@gmail.com>
2020-08-06 12:45:11 -0300

    trabalhando nas rotas

---
+++ src/routes.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import db from './database/connection';
+
+const routes = express.Router();
+
+
+routes.post('/classes',async(request,response) => {
+    const {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        cost,
+        schedule
+    } = request.body;
+
+    const insertedUsersIds = await db('users').insert({
+        name,avatar,bio,whatsapp
+    })
+
+    const user_id = insertedUsersIds[0];
+
+    const insertedClassesIds = await db('classes').insert({
+        subject,cost,user_id
+    })
+
+    const class_id = insertedClassesIds[0]
+
+    return response.send();
+})
+
+
+export default routes;
