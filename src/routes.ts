f361941644a52a3142731119ea20bd55feae9a8a
PabloSSena <pablosilvasena@gmail.com>
2020-08-06 23:26:07 -0300

    Muita coisa feita

--- src/routes.ts
+++ src/routes.ts
@@ -1,65 +1,15 @@
 import express from 'express';
-import db from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutes';
-
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 const routes = express.Router();
 
-interface ScheduleItem{
-    week_day:number,
-    from:string,
-    to:string
-}
+const classesControllers =  new ClassesController();
+const connectionsControllers = new ConnectionsController()
 
-routes.post('/classes',async(request,response) => {
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = request.body;
-
-    const trx = await db.transaction(  )
-
-    try{
-
-        const insertedUsersIds = await trx('users').insert({
-            name,avatar,bio,whatsapp
-        })
-
-        const user_id = insertedUsersIds[0];
-
-        const insertedClassesIds = await trx('classes').insert({
-            subject,cost,user_id
-        })
-
-        const class_id = insertedClassesIds[0]
-
-        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-            return {
-            class_id,
-            week_day:scheduleItem.week_day,
-            from: convertHourToMinutes(scheduleItem.from),
-            to: convertHourToMinutes(scheduleItem.to),
-            };
-        })
-
-        await trx('class_schedule').insert(classSchedule);
-
-        await trx.commit();
-
-        return response.status(201).send();
-    } catch(err){
-
-        await trx.rollback();
-
-        return response.status(400).json({
-            error:"Unexpected error while creating new classs"
-        })
-    }
-})
+routes.get('/classes',classesControllers.index);
+routes.post('/classes',classesControllers.create);
+routes.post('/connections',connectionsControllers.create);
+routes.get('/connections',connectionsControllers.index);
 
 
 export default routes;
